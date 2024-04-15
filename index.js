0928cde1bbad0d12259e34a061305e80b690bfa7
Aleksandr Kubenko <151834458+Gumeynik@users.noreply.github.com>
2024-04-15 15:30:36 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-function getRandomDogImage() {
-    fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Ошибка HTTP: ' + response.status);
-        }
-        return response.json();
-    })
-    .then(data => {
-        const dog = data.message;
-        const container = document.querySelector('#dog-image-container'); // Используйте более конкретный селектор
-        const img = document.createElement('img');
-        img.src = dog;
-        container.appendChild(img); // Используйте appendChild для добавления нового изображения в контейнер
-    })
-    .catch(error => {
-        console.error('Произошла ошибка при получении данных:', error);
-    });
-}
+// function getRandomDogImage() {
+//     fetch('https://dog.ceo/api/breeds/image/random')
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error('Ошибка HTTP: ' + response.status);
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+//         const dog = data.message;
+//         const container = document.querySelector('#dog-image-container'); // Используйте более конкретный селектор
+//         const img = document.createElement('img');
+//         img.src = dog;
+//         container.appendChild(img); // Используйте appendChild для добавления нового изображения в контейнер
+//     })
+//     .catch(error => {
+//         console.error('Произошла ошибка при получении данных:', error);
+//     });
+// }
 
-window.onload = getRandomDogImage;
+// window.onload = getRandomDogImage;
+fetch('https://dog.ceo/api/breeds/image/random')
+.then(data => data.json())
+.then((res) => {
+    if (res.status !== 'success') {
+        return;
+    }
+    document.querySelector('#dog-image-container').innerHTML = `
+    <img class="img" src='${res.message}'>
+    `
+}
+)
